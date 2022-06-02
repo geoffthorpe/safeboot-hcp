2cb7fda70a7ca2550f830e4d014f0298ee5b875c
Geoffrey Thorpe <geoffrey@twosigma.com>
2022-06-02 17:29:21 +1200

    enrollsvc: combine common.sh and common_defs.sh
    These had been split out in order to take a subset of definitions and
    functions, that deal with the enrolldb layout, and embed that code in the
    enrolldb itself. I.e. so the attestsvc could interrogate the enrolldb
    using the same definitions as the enrollsvc was using, and in theory this
    would allow the layout to change. As with the versioning support, this
    didn't really pan out and is now just complexity with no benefit, so it is
    removed.
    
    Signed-off-by: Geoffrey Thorpe <geoffrey@twosigma.com>

--- src/apps/enrollsvc/common.sh
+++ src/apps/enrollsvc/common.sh
@@ -1,12 +1,3 @@
-# This file (common.sh) contains definitions required within the git container
-# for operating on the repository, e.g. dropping privs to the db_user or
-# flask_user, taking and releasing the lockfile, etc. The conventions for the
-# repository's directory layout and the file contents are put in a seperate
-# file, common_defs.sh, which is sourced by this file _and is actually
-# committed to the repo itself_. This is so that common_def.s is replicated to
-# the attestation service nodes, so that they make the same directory-layout
-# (and semantic) assumptions.
-
 . /hcp/common/hcp.sh
 
 set -e
@@ -127,12 +118,12 @@ function expect_flask_user {
 }
 
 function drop_privs_db {
-	# The only thing we need to whitelist is DB_IN_SETUP, which is used by
-	# setup_enrolldb.sh to suppress common_defs.sh's test for an existing
-	# db. We could've written this to /etc/environment too, but this case
-	# only applies during one-time initialization whereas the other
-	# settings apply longer term. (The fact this is only used as we drop
-	# from root to non-root also means it's OK.)
+	# The only thing we need to whitelist is DB_IN_SETUP, to suppress our
+	# checks for the existence of a directory at $(EK_PATH). I.e.
+	# run_repl.sh uses this when polling to wait for the database to be
+	# initialized (without which our check would cause it to exit), and
+	# run_mgmt.sh uses it when making the same check and when subsequently
+	# initializing the database.
 	exec su --whitelist-environment DB_IN_SETUP -c "$*" - db_user
 }
 
@@ -149,28 +140,27 @@ function repo_cmd_unlock {
 	rm -f $REPO_LOCKPATH
 }
 
-# The remaining functions are in a separate file because they form part of the git
-# repo itself. (So that the attestation servers, which clone and use the repo
-# in a read-only capacity, always use the same assumptions.) But to avoid
-# chicken and eggs, we source the original (in the root directory, put there by
-# Dockerfile) rather than the copy in the repo.
-
-. /hcp/enrollsvc/common_defs.sh
-
-# Except ... we also provide a reverse-lookup (hostname to ekpubhash) in a
-# single file that the attestation service itself isn't supposed to need. We
-# put the relevant definitions here (rather than in common_defs.h) to emphasize
-# this point.
+# The remaining functions are used for navigating and manipulating the
+# enrollment database.
 #
-# TODO: we could do much better than the following. As the size of the dataset
-# grows, the adds and deletes to the reverse-lookup table will dominate, as
-# will memory and file-system thrashing (due to the need to copy and filter
-# copies of the table inside the critical section). As with elsewhere, we make
-# do with a simple but easy-to-validate solution for now, and mark this for a
-# smarter implementation when there is enough time and focus to not make a mess
-# of it (and once things are running at a scale that can detect problems).
+# TODO: we could do much better than the HN2EK stuff. The enrollmentdb (as
+# managed in git) inherently maps TPMs (a hash of their 'ekpub') to their
+# corresponding hosts (hostname and the assets enrolled for it). The HN2EK
+# mechanism sits outside that, using a single file to provide a reverse
+# mapping. Reverse-mapping isn't a common requirement and needn't be
+# performant. Also, adding and deleting to/from the enrolldb database involves
+# stacks of scripts, lots of crypto and I/O, and operations on a git repo.
+# Updating the HN2EK file should be small in comparison.
 #
-# Each line of this file is a space-separated 2-tuple of;
+# HOWEVER, those add/delete operations on the enrolldb are constant-time,
+# however heavy they might be, whereas the updates to the HN2EK file are at
+# least linear with fleet size, so it's conceivable that it needs a smarter
+# approach if very large fleet sizes cause the HN2EK overheads to dominate.
+# copies of the table inside the critical section).
+
+
+# The HN2EK reverse-mapping table is pretty simple. Each line of this file is a
+# space-separated 2-tuple of;
 # - the reversed hostname (per 'rev')
 # - the ekpubhash (truncated to 32 characters if appropriate, i.e. to match the
 #   name of the per-TPM sub-sub-sub-drectory in the ekpubhash/ directory tree).
@@ -178,3 +168,91 @@ function repo_cmd_unlock {
 # The initially-empty file
 HN2EK_BASENAME=hn2ek
 HN2EK_PATH=$REPO_PATH/$HN2EK_BASENAME
+
+# The following definitions are for the git-based enrolldb.
+
+# EK_PATH must point to the 'ekpubhash' directory (in the "enrolldb.git"
+# repo/clone), unless (of course) we are being sourced by the script that is
+# creating the repo.
+if [[ -z "$EK_PATH" || ! -d "$EK_PATH" ]]; then
+	if [[ -z "$DB_IN_SETUP" ]]; then
+		echo "Error, EK_PATH must point to the ekpubhash lookup tree" >&2
+		exit 1
+	fi
+fi
+
+# ekpubhash must consist only of lower-case hex, and be at least 16 characters
+# long (8 bytes)
+function check_ekpubhash {
+	(echo "$1" | egrep -e "^[0-9a-f]{16,}$" > /dev/null 2>&1) ||
+		(echo "Error, malformed ekpubhash" >&2 && exit 1) || exit 1
+}
+
+# the prefix version can be any length (including empty)
+function check_ekpubhash_prefix {
+	(echo "$1" | egrep -e "^[0-9a-f]*$" > /dev/null 2>&1) ||
+		(echo "Error, malformed ekpubhash" >&2 && exit 1) || exit 1
+}
+
+# hostname must consist only of alphanumerics, periods ("."), hyphens ("-"),
+# and underscores ("_"). TODO: our code actually allows empty hostnames, which
+# is why the "_suffix" version doesn't do anything special. (The _suffix
+# version certainly _should_ accept the empty case, because it's a suffix match
+# for a query), but we should probably require hostnames to be non-empty, and
+# probably satisfy some other sane-hostname constraints.
+function check_hostname {
+	(echo "$1" | egrep -e "^[0-9a-zA-Z._-]*$" > /dev/null 2>&1) ||
+		(echo "Error, malformed hostname" >&2 && exit 1) || exit 1
+}
+function check_hostname_suffix {
+	check_hostname $1
+}
+
+# We use a 3-ply directory hierarchy for storing per-TPM state, indexed by the
+# "ekpubhash" of that TPM (or more accurately, the hexidecimal string
+# representation of the ekpubhash in text form - 4 bits per ASCII character).
+# The first ply uses the first 2 hex characters as a directory name, for a
+# split of 256. The second ply uses the first 6 characters as a directory name,
+# meaning 4 new characters of uniqueness for a further split of 65536,
+# resulting in a total split of ~16 million. The last ply uses the first 32
+# characters of the ekpubhash, with a working assumption that this (128-bits)
+# is enough to establish TPM uniqueness, and no collision-handling is employed
+# beyond that. That 3rd-ply (per-TPM) directory contains individual files for
+# each attribute to be associated with the TPM, including 'ekpubhash' itself
+# (full-length), and 'hostname'.
+
+# Given an ekpubhash ($1), figure out the corresponding 3-ply of directories.
+# Outputs;
+#   PLY1, PLY2, PLY3: directory names
+#   FPATH: full path
+function ply_path_add {
+	PLY1=`echo $1 | cut -c 1,2`
+	PLY2=`echo $1 | cut -c 1-6`
+	PLY3=`echo $1 | cut -c 1-32`
+	FPATH="$EK_PATH/$PLY1/$PLY2/$PLY3"
+}
+
+# Given an ekpubhash prefix ($1), figure out the wildcard to match on all the
+# matching per-TPM directories. (If using "ls", don't forget to use the "-d"
+# switch!)
+# Outputs;
+#   FPATH: full path with wildcard pattern
+function ply_path_get {
+	len=${#1}
+	if [[ $len -lt 2 ]]; then
+		FPATH="$EK_PATH/$1*/*/*"
+	else
+		PLY1=`echo $1 | cut -c 1,2`
+		if [[ $len -lt 6 ]]; then
+			FPATH="$EK_PATH/$PLY1/$1*/*"
+		else
+			PLY2=`echo $1 | cut -c 1-6`
+			if [[ $len -lt 32 ]]; then
+				FPATH="$EK_PATH/$PLY1/$PLY2/$1*"
+			else
+				PLY3=`echo $1 | cut -c 1-32`
+				FPATH="$EK_PATH/$PLY1/$PLY2/$PLY3"
+			fi
+		fi
+	fi
+}
--- src/apps/enrollsvc/init_repo.sh
+++ src/apps/enrollsvc/init_repo.sh
@@ -13,7 +13,6 @@ touch .git/git-daemon-export-ok
 touch $HN2EK_PATH
 mkdir $EK_BASENAME
 touch $EK_BASENAME/do_not_remove
-cp /hcp/enrollsvc/common_defs.sh .
 git add .
 git commit -m "Initial commit"
 git log
