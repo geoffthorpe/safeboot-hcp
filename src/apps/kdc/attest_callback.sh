64a6cbb408497d3bf377fd6a0333fd148873d4a7
Geoffrey Thorpe <geoffrey@twosigma.com>
2022-06-23 10:49:32 -0400

    run_client/usecase: unify attestation callbacks
    A single common attest_callback handles the various asset types in a
    canonical way.
    
    Signed-off-by: Geoffrey Thorpe <geoffrey@twosigma.com>

--- src/apps/kdc/attest_callback.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ ! -f hostcert-pkinit-kdc.pem ]]; then
-	echo "Error, 'hostcert-pkinit-kdc.pem' missing" >&2
-	exit 1
-fi
-echo "Retrieved KDC certificate"
-cp hostcert-pkinit-kdc.pem $HCP_KDC_STATE/etc/kdc-cert.pem
-chmod 400 $HCP_KDC_STATE/etc/kdc-cert.pem
