3f29c0eb17e63504a11ecc052c6fb457cd392c05
Geoffrey Thorpe <geoffrey@twosigma.com>
2022-07-06 13:48:55 -0400

    base: silence 'chowner' unless V is defined
    Signed-off-by: Geoffrey Thorpe <geoffrey@twosigma.com>

--- src/base/3platform-context/chowner.sh
+++ src/base/3platform-context/chowner.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-echo "CHOWNER, running in $2, using reference=$1"
+[[ -z $V ]] || echo "CHOWNER, running in $2, using reference=$1"
 
 MYUID=$(stat --format=%u $1)
 MYGID=$(stat --format=%g $1)
 
-echo "UID=$MYUID"
-echo "GID=$MYGID"
-
-find $2 ! -uid $MYUID -exec chown -h -v $MYUID:$MYGID {} \;
-
+if [[ -z $V ]]; then
+	find $2 ! -uid $MYUID -exec chown -h $MYUID:$MYGID {} \;
+else
+	echo "UID=$MYUID"
+	echo "GID=$MYGID"
+	find $2 ! -uid $MYUID -exec chown -h -v $MYUID:$MYGID {} \;
+fi
