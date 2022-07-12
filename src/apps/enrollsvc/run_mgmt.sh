d8ba54fd99c0376705547feb5ecc512b61510bef
Geoffrey Thorpe <geoffrey@twosigma.com>
2022-07-11 22:38:24 -0400

    enrollsvc: use HTTPS and support being TPM/enrollment-based!
    - if HCP_ENROLLSVC_ENABLE_SWTPM is defined, initialize a local (not
      side-car) swtpmsvc instance and start it. Note, this uses "orchestrator"
      but only for creating the TPM, its enrollment support is deliberately
      disabled. (This has to run prior to the API being available.)
    - if HCP_ENROLLSVC_ENABLE_SWTPM_SELFENROLL is defined, it will furthermore
      enroll its own TPM into the enrollment database without going via the
      management (web) API. (It won't be available yet.)
    - if HCP_ENROLLSVC_ENABLE_ATTEST is defined, it will wait until it can
      successfully attest and obtain credentials. These will include the HTTPS
      server certificate and the issuer trust root (for validating client
      certificates).
    
    Signed-off-by: Geoffrey Thorpe <geoffrey@twosigma.com>

--- src/apps/enrollsvc/run_mgmt.sh
+++ src/apps/enrollsvc/run_mgmt.sh
@@ -1,10 +1,67 @@
 #!/bin/bash
 
-. /hcp/enrollsvc/common.sh
+source /hcp/enrollsvc/common.sh
 
 expect_root
 
-# Do common.sh-style things that are specific to the replication sub-service.
+if [[ -n $HCP_ENROLLSVC_ENABLE_SWTPM ]]; then
+
+	if [[ ! -f "$HCP_SWTPMSVC_STATE/tpm/ek.pub" ]]; then
+		echo "Creating local swtpm instance for $HCP_HOSTNAME"
+		export HCP_ORCHESTRATOR_JSON=$(mktemp)
+		cat > $HCP_ORCHESTRATOR_JSON << EOF
+{ "fleet": [ {
+	"name": "emgmt",
+	"tpm_create": true,
+	"tpm_path": "$HCP_SWTPMSVC_STATE",
+	"enroll": false
+} ] }
+EOF
+		/hcp/tools/run_orchestrator.sh
+		rm $HCP_ORCHESTRATOR_JSON
+	fi
+
+	echo "Starting local swtpm instance for $HCP_HOSTNAME"
+	# Background the swtpm instance. Note, we don't want anything mounted
+	# at $HCP_EMGMT_ATTEST_TCTI_SOCKDIR to facilitate access to a swtpm
+	# container precisely because we're choosing to have it running
+	# locally/internally. We need to create the directory here instead.
+	mkdir -p $HCP_EMGMT_ATTEST_TCTI_SOCKDIR
+	/hcp/swtpmsvc/run_swtpm.sh &
+
+	# Handle self-enrollment of private swtpmsvc
+	if [[ -n $HCP_ENROLLSVC_ENABLE_SWTPM_SELFENROLL &&
+			! -f "$HCP_ENROLLSVC_STATE/self-enrolled" ]]; then
+		echo "Self-enrolling swtpm EK"
+		chmod 644 "$HCP_SWTPMSVC_STATE/tpm/ek.pub"
+		(drop_privs_db /hcp/enrollsvc/self_enroll.sh)
+		touch "$HCP_ENROLLSVC_STATE/self-enrolled"
+	fi
+fi
+
+if [[ -n $HCP_ENROLLSVC_ENABLE_ATTEST ]]; then
+	# Run the attestation and get our assets
+	# Note, run_client is not a service, it's a utility, so it doesn't
+	# retry forever waiting for things to be ready to succeed. We, on the
+	# other hand, _are_ a service, so we need to be more forgiving.
+	echo "Running attestation client"
+	attestlog=$(mktemp)
+	if ! /hcp/tools/run_client.sh 2> $attestlog; then
+		echo "Warning: the attestation client lost patience, error output follows;" >&2
+		cat $attestlog >&2
+		rm $attestlog
+		echo "Warning: suppressing error output from future attestation attempts" >&2
+		attestation_done=
+		until [[ -n $attestation_done ]]; do
+			echo "Warning: waiting 10 seconds before retring attestation" >&2
+			sleep 10
+			echo "Retrying attestation" >&2
+			/hcp/tools/run_client.sh 2> /dev/null && attestation_done=yes
+		done
+	fi
+fi
+
+# Do common.sh-style things that are specific to the management sub-service.
 if [[ ! -f $SIGNING_KEY_PUB || ! -f $SIGNING_KEY_PRIV ]]; then
 	echo "Error, SIGNING_KEY_{PUB,PRIV} ($SIGNING_KEY_PUB,$SIGNING_KEY_PRIV) do not contain valid creds" >&2
 	exit 1
---
+++ src/apps/enrollsvc/self_enroll.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source /hcp/enrollsvc/common.sh
+
+expect_db_user
+
+MYPROFILE=$(cat - << EOF
+{
+	"gencert-hxtool": {
+		"list": "default-pkinit-client default-https-client default-https-server"
+	}
+}
+EOF
+)
+
+MYOUT=$(mktemp)
+
+if ! /hcp/enrollsvc/op_add.sh \
+		"$HCP_SWTPMSVC_STATE/tpm/ek.pub" \
+		"$HCP_ENROLLSVC_SELFENROLL_HOSTNAME" \
+		"$MYPROFILE" > "$MYOUT" 2>&1; then
+	echo "Error, self-enrollment failed. Trace output;" >&2
+	cat "$MYOUT" >&2
+	exit 1
+fi
+true
