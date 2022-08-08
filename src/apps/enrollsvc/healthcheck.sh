c54c1b6e690024a9cace57f257962d5c3f690f09
Geoffrey Thorpe <geoffrey@twosigma.com>
2022-08-08 17:42:03 -0400

    enrollsvc: fix healthcheck regression
    cert/key naming changed recently, but this healthcheck wasn't updated.
    Also, only pass the --cert arg when it's relevant.
    
    Signed-off-by: Geoffrey Thorpe <geoffrey@twosigma.com>

--- src/apps/enrollsvc/healthcheck.sh
+++ src/apps/enrollsvc/healthcheck.sh
@@ -5,11 +5,13 @@ source /hcp/common/hcp.sh
 hcp_pre_launch
 
 URL=${HCP_EMGMT_HOSTNAME}.${HCP_FQDN_DEFAULT_DOMAIN}
+CERTARG=""
 if [[ -n $HCP_EMGMT_ENABLE_NGINX ]]; then
 	URL=https://$URL:8443
+	CERTARG="--cert /etc/ssl/hostcerts/hostcert-default-https-client-key.pem"
 else
 	URL=http://$URL:5000
 fi
 URL=$URL/healthcheck
 
-curl -f -G --cert /etc/ssl/hostcerts/hostcert-default-https-client.pem $URL
+curl -f -G $CERTARG $URL
