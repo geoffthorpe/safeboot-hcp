d8cb21054d625bc1b7878fb011509b6209f22751
Geoffrey Thorpe <geoffrey@twosigma.com>
2022-08-08 18:01:58 -0400

    policysvc: fix case where mounted files are only root-visible
    The policysvc runs in uwsgi, which drops privs before handling traffic. In
    some docker configurations (esp. "rootless"), files in host mounts aren't
    world-readable. That's what happens with the "usecase/policy.json" file
    that policysvc reads. Quick and nasty solution: have the startup script
    copy it to a temp file, chmod the temp file to be world-readable, and
    repoint the HCP_POLICYSVC_JSON env-var to point to that temp file.
    
    Signed-off-by: Geoffrey Thorpe <geoffrey@twosigma.com>

--- src/apps/policysvc/run_policy.sh
+++ src/apps/policysvc/run_policy.sh
@@ -9,6 +9,16 @@ echo "Running 'policysvc' service"
 if [[ ! -f $HCP_POLICYSVC_UWSGI_INI ]]; then
 	echo "Error, HCP_POLICYSVC_UWSGI_INI ($HCP_POLICYSVC_UWSGI_INI) isn't available" >&2
 fi
+if [[ ! -f $HCP_POLICYSVC_JSON ]]; then
+	echo "Error, HCP_POLICYSVC_JSON ($HCP_POLICYSVC_JSON) isn't available" >&2
+fi
+
+# Ugh. If we're running in an exotic docker configuration (eg. rootless), the JSON input might
+# be in a mount that the www-data user can't read. Another hack to the rescue;
+newjson=$(mktemp)
+cp "$HCP_POLICYSVC_JSON" "$newjson"
+chmod 644 "$newjson"
+export HCP_POLICYSVC_JSON="$newjson"
 
 echo "policysvc, running uwsgi with the python flask app"
 exec uwsgi_python3 --ini $HCP_POLICYSVC_UWSGI_INI
