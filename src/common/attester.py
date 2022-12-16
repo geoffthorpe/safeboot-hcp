30e90a4b27583ea5aefcf1e562d8adea537e02f4
Geoffrey Thorpe <geoffrey@twosigma.com>
2022-12-16 12:37:10 -0500

    attester: complement 'period' with a 'retry' setting
    It makes sense for the 'period' attribute to be long, as a successful
    attestation serves to get cred/config updates, which may only be necessary
    on periods measured in hours or days. But when bringing up services out of
    order, if a first attestation fails (because attestsvc lost the start-up
    race, or because TPMs aren't done being created and/or enrolled, ...) we
    don't want to wait long before a retry.
    
    Signed-off-by: Geoffrey Thorpe <geoffrey@twosigma.com>

--- src/common/attester.py
+++ src/common/attester.py
@@ -10,10 +10,12 @@ from hcp_common import log, bail, current_tracefile, hcp_config_extract, \
 	hcp_config_scope_set, hcp_config_scope_get, hcp_config_scope_shrink
 
 _period = hcp_config_extract('.attester.period', must_exist = True)
+_retry = hcp_config_extract('.attester.retry', or_default = True, default = _period)
 try:
 	period = int(_period)
+	retry = int(_retry)
 except ValueError as e:
-	log(f"ERROR: .attester.period ({_period}) must be a number")
+	log(f"ERROR: .attester.{period,retry} ({_period},{_retry}) must be numbers")
 	log(f"{e}")
 	sys.exit(1)
 cmd_args = [ '/hcp/tools/run_client.sh' ]
@@ -31,5 +33,9 @@ while True:
 		res = c.returncode
 	except Exception as e:
 		log(f"Warning, exception: {e}")
+		res = -1
 	log(f"Command exited with code={res}")
-	time.sleep(period)
+	if res == 0:
+		time.sleep(period)
+	else:
+		time.sleep(retry)
